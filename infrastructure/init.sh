7025b24087c0d76e5d9d85287f683c5cb4d6a660
marioharper <harpmari12@gmail.com>
2018-01-15 12:31:31 -0600

    change application name, fix quotes around ref

--- infrastructure/init.sh
+++ infrastructure/init.sh
@@ -6,4 +6,4 @@ read GITHUB_USER_NAME
 echo "Enter a personal GitHub access token. Generate one here: https://github.com/settings/tokens (scopes needed: admin:repo_hook, repo)"
 read GITHUB_OAUTH_TOKEN
 
-aws --region us-east-2 cloudformation create-stack --stack-name "aftp-pipeline" --template-body file://pipeline.yaml --parameters "ParameterKey=GitHubUserName,ParameterValue=${GITHUB_USER_NAME}" "ParameterKey=GitHubOAuthToken,ParameterValue=${GITHUB_OAUTH_TOKEN}" --capabilities CAPABILITY_IAM
+aws --region us-east-2 cloudformation create-stack --stack-name "node-microservice-ci-cd" --template-body file://pipeline.yaml --parameters "ParameterKey=GitHubUserName,ParameterValue=${GITHUB_USER_NAME}" "ParameterKey=GitHubOAuthToken,ParameterValue=${GITHUB_OAUTH_TOKEN}" --capabilities CAPABILITY_IAM
