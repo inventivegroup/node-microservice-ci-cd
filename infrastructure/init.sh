17fbb283d5adb7981b0c7b198503e1b51b830b42
marioharper <harpmari12@gmail.com>
2018-01-15 12:04:38 -0600

    add pipeline as code

---
+++ infrastructure/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+echo "Enter your GitHub user name."
+read GITHUB_USER_NAME
+
+echo "Enter a personal GitHub access token. Generate one here: https://github.com/settings/tokens (scopes needed: admin:repo_hook, repo)"
+read GITHUB_OAUTH_TOKEN
+
+aws --region us-east-2 cloudformation create-stack --stack-name "aftp-pipeline" --template-body file://pipeline.yaml --parameters "ParameterKey=GitHubUserName,ParameterValue=${GITHUB_USER_NAME}" "ParameterKey=GitHubOAuthToken,ParameterValue=${GITHUB_OAUTH_TOKEN}" --capabilities CAPABILITY_IAM
